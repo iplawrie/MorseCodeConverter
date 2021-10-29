a57628c3b594a63c813ec050e0fbb80d2c87aff0
iplawrie <55101286+iplawrie@users.noreply.github.com>
2021-10-29 13:36:04 -0400

    python code for ui

---
+++ ui_QT.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'QTOrXzpA.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1020, 687)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.horizontalLayoutWidget = QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
+        self.horizontalLayoutWidget.setGeometry(QRect(10, 20, 961, 581))
+        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setSpacing(10)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setSizeConstraint(QLayout.SetNoConstraint)
+        self.verticalLayout_2.setContentsMargins(5, 5, 5, 5)
+        self.Input = QTextEdit(self.horizontalLayoutWidget)
+        self.Input.setObjectName(u"Input")
+
+        self.verticalLayout_2.addWidget(self.Input)
+
+        self.Output = QTextEdit(self.horizontalLayoutWidget)
+        self.Output.setObjectName(u"Output")
+
+        self.verticalLayout_2.addWidget(self.Output)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout_2)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.verticalLayout.setContentsMargins(5, 5, 5, 5)
+        self.submit = QPushButton(self.horizontalLayoutWidget)
+        self.submit.setObjectName(u"submit")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.submit.sizePolicy().hasHeightForWidth())
+        self.submit.setSizePolicy(sizePolicy)
+
+        self.verticalLayout.addWidget(self.submit)
+
+        self.playsound = QPushButton(self.horizontalLayoutWidget)
+        self.playsound.setObjectName(u"playsound")
+        sizePolicy.setHeightForWidth(self.playsound.sizePolicy().hasHeightForWidth())
+        self.playsound.setSizePolicy(sizePolicy)
+
+        self.verticalLayout.addWidget(self.playsound)
+
+        self.comboBox = QComboBox(self.horizontalLayoutWidget)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+        sizePolicy.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
+        self.comboBox.setSizePolicy(sizePolicy)
+        self.comboBox.setMinimumSize(QSize(20, 20))
+
+        self.verticalLayout.addWidget(self.comboBox)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.submit.setText(QCoreApplication.translate("MainWindow", u"Convert ", None))
+        self.playsound.setText(QCoreApplication.translate("MainWindow", u"Play Sound ", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"New Item", None))
+        self.comboBox.setItemText(1, QCoreApplication.translate("MainWindow", u"New Item", None))
+        self.comboBox.setItemText(2, QCoreApplication.translate("MainWindow", u"New Item", None))
+
+        self.comboBox.setCurrentText(QCoreApplication.translate("MainWindow", u"New Item", None))
+    # retranslateUi
+
