ef5f72c306f44a40ddf91f4213419fc2975cae14
Ian Lawrie <iplawrie@mtu.edu>
2021-10-29 13:51:38 -0400

    10/29/2021

--- UIsetup.py
+++ UIsetup.py
@@ -1,17 +1,7 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'QTOrXzpA.ui'
-##
-## Created by: Qt User Interface Compiler version 5.15.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
 
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
